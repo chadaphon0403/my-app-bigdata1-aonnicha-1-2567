a7da8f39c78ffaa970be565a86db0772828490e0
“Aonnicha071244 <aonnicha987@gmail.com>
2024-07-19 10:34:41 +0700

    first commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
                 <a href="/about">เกี่ยวกับ</a>
               </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
-                <a href="/ai">AI ตรวจวินิจฉัยโรค                                                                                           </a>
+                <a href="/ai">AI ตรวจวินิจฉัยโรค </a>
               </li>
               <li className="text-stone-600 hover:text-blue-600 hover:font-bold font-medium mx-4 my-1">
                 <a href="#">ChatGPT</a>
